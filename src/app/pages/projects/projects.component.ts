e843fc1a49d5ddded5ec9e84a5333cfeb84adee6
Juan Gutierrez <juanri98@hotmail.com>
2024-06-13 14:13:27 +0200

    proyects Page

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from './productservice';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
-export class ProjectsComponent {
-
+export class ProjectsComponent implements OnInit {
+  products: any[] = [];
+  responsiveOptions: any[] = [];
+  constructor(private productService: ProductService) {}
+  ngOnInit() {
+    this.productService.getProducts().then((products: any) => {
+      this.products = products;
+    });
+    this.responsiveOptions = [
+      {
+        breakpoint: '1199px',
+        numVisible: 1,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '991px',
+        numVisible: 2,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '767px',
+        numVisible: 1,
+        numScroll: 1,
+      },
+    ];
+  }
 }
