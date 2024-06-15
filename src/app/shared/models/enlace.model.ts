01071e16b817ebfbc84e606edc2601a2e93583eb
Juan Gutierrez <juanri98@hotmail.com>
2024-06-15 15:27:58 +0200

    Deploy

--- src/app/shared/models/enlace.model.ts
+++
@@ -1,4 +0,0 @@
-export interface Enlace {
-  ruta: string;
-  texto: string;
-}
--- src/app/shared/models/timeline-event.model.ts
+++
@@ -1,6 +0,0 @@
-export interface TimelineEvent {
-  job: string;
-  company: string;
-  country?: string;
-  date: string;
-}
